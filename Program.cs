14573a52c8817bca934372844c71b7c6561fb1ad
NThacker5246 <nytrecover@gmail.com>
2026-02-28 20:48:43 +0400

    Added Server.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Globalization;
+using System.Text;
+using System.Text.Json;
+
+Server sv = new Server(80, 2048);
+while(true){
+	sv.TickServer();
+}
--- obj/Debug/net8.0/AlgTexter.AssemblyInfo.cs
+++ obj/Debug/net8.0/AlgTexter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgTexter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634d9b80bd797b48de547e53006bd858d29c8634")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgTexter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgTexter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
